9176634e87cf80e2f51ef3eb6b28fd8d5dc932e5
LicentiousCad <kstoots1@kent.edu>
2016-03-15 17:00:09 -0400

    Create Lab5_Figures.h

---
+++ Lab5_Figures.h
@@ -0,0 +1,14 @@
+// Prints out asterisks in pretty shapes!
+// 02/19/2016
+
+#ifndef figures_H
+#define figures_H
+// Makes a regular right triangle
+void leftTriangle(int tracker);
+// Makes the reversed right triangle
+void rightTriangle(int tracker);
+// Makes the filled square
+void filledSquare(int tracker);
+// Makes the hollow square
+void hollowSquare(int tracker);
+#endif
